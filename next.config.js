7e37b27cd90fd9206aba7f8ce214ddf11fc12931
lhousaine <l.ouarhou.ensa@gmail.com>
2022-07-03 10:19:31 +0100

    feat(i18n config): add i18n configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  i18n: {
+    /**
+     * Provide the locales you want to support in your application
+     */
+    locales: ['en-US', 'fr-FR', 'es-ES'],
+    /**
+     * This is the default locale you want to be used when visiting
+     * a non-locale prefixed path e.g. `/hello`
+     */
+    defaultLocale: 'en-US',
+  },
+};
